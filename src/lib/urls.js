cec8ec36c8e5be7f9bd39ef62831ade8a3ac0965
WlodekM <75479945+WlodekM@users.noreply.github.com>
2023-04-21 10:57:18 +0300

    Update urls.js

--- src/lib/urls.js
+++ src/lib/urls.js
@@ -6,6 +6,6 @@ export const linkUrl =
 	localStorage.getItem("meower_linkurl") ||
 	"wss://server.streamilator.tk/";
 export const apiUrl =
-	localStorage.getItem("meower_apiurl") || "https://server.streamilator.tk/v0/cloudlink/";
+	localStorage.getItem("meower_apiurl") || "https://api.streamilator.tk/";
 export const encodeApiURLParams =
 	localStorage.getItem("meower_encodeapi") || false;
