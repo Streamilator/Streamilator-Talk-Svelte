45e5b6ab098ebfc9845a5df12a179d123dc18c57
WlodekM <75479945+WlodekM@users.noreply.github.com>
2023-06-09 09:52:51 +0300

    Update DevTools.svelte

--- src/lib/modals/DevTools.svelte
+++ src/lib/modals/DevTools.svelte
@@ -20,109 +20,109 @@
 	import * as clm from "../clmanager.js";
 	import {apiUrl, encodeApiURLParams} from "../urls.js";
 
-	const PFP_COUNT = 38;
-	var pfp_temp = $user.pfp_data
-	var result_eval, code_eval
+// 	const PFP_COUNT = 38;
+// 	var pfp_temp = $user.pfp_data
+// 	var result_eval, code_eval
 
-	const pfps = new Array(PFP_COUNT).fill().map((_, i) => i + 1);
-	pfps.push(-1, 500, 101, 102, 404) //add secret pfps
-	let pfpSwitcher = false;
+// 	const pfps = new Array(PFP_COUNT).fill().map((_, i) => i + 1);
+// 	pfps.push(-1, 500, 101, 102, 404) //add secret pfps
+// 	let pfpSwitcher = false;
 
-	async function loadProfile() {
-		let path = `users/${$profileClicked}`;
-		if (encodeApiURLParams) path = encodeURIComponent(path);
-		const resp = await fetch(`${apiUrl}${path}`);
-		if (!resp.ok) {
-			throw new Error("Response code is not OK; code is " + resp.status);
-		}
-		const json = await resp.json();
-		return json;
-	}
+// 	async function loadProfile() {
+// 		let path = `users/${$profileClicked}`;
+// 		if (encodeApiURLParams) path = encodeURIComponent(path);
+// 		const resp = await fetch(`${apiUrl}${path}`);
+// 		if (!resp.ok) {
+// 			throw new Error("Response code is not OK; code is " + resp.status);
+// 		}
+// 		const json = await resp.json();
+// 		return json;
+// 	}
 
-	/**
-	 * Saves the user profile, and also clears its cache entry.
-	 */
-	function save() {
-		if ($profileCache[$user.name]) {
-			const _profileCache = $profileCache;
-			delete _profileCache[$user.name];
-			profileCache.set(_profileCache);
-		}
+// 	/**
+// 	 * Saves the user profile, and also clears its cache entry.
+// 	 */
+// 	function save() {
+// 		if ($profileCache[$user.name]) {
+// 			const _profileCache = $profileCache;
+// 			delete _profileCache[$user.name];
+// 			profileCache.set(_profileCache);
+// 		}
 
-		clm.updateProfile();
-	}
+// 		clm.updateProfile();
+// 	}
 
-	let pfpOverflow = false;
-	var target_page = "groupcat"
-	$: {
-		const pfp = $user.pfp_data;
-		let hiddenpfps = [-1, 500, 101, 102]
-		pfpOverflow = pfp < 0 || pfp > PFP_COUNT && !(hiddenpfps.includes(pfp));
-	}
-	var spamn = 10
-	function spamlive() {
-		for (var count = 0; count < spamn; count++) {
-        link.send({
-            "cmd": "direct",
-            "val": {
-                "cmd": "set_chat_state",
-                "val": {
-                    "state": 1,
-                    "chatid": "livechat"
-                }
-            },
-        })
-    }
-	}
+// 	let pfpOverflow = false;
+// 	var target_page = "groupcat"
+// 	$: {
+// 		const pfp = $user.pfp_data;
+// 		let hiddenpfps = [-1, 500, 101, 102]
+// 		pfpOverflow = pfp < 0 || pfp > PFP_COUNT && !(hiddenpfps.includes(pfp));
+// 	}
+// 	var spamn = 10
+// 	function spamlive() {
+// 		for (var count = 0; count < spamn; count++) {
+//         link.send({
+//             "cmd": "direct",
+//             "val": {
+//                 "cmd": "set_chat_state",
+//                 "val": {
+//                     "state": 1,
+//                     "chatid": "livechat"
+//                 }
+//             },
+//         })
+//     }
+// 	}
   
-	let postErrors = "";
+// 	let postErrors = "";
 	
-	function goto(newPage, resetScroll = true) {
-		if (
-			!$user.name &&
-			newPage !== "home" &&
-			newPage !== "settings" &&
-			newPage !== "changelog" &&
-			newPage !== "search"
-		) {
-			modalPage.set("signup");
-			modalShown.set(true);
-			return;
-		}
-		if (resetScroll) {
-			window.scrollTo(0, 0);
-		}
-		if ($page === "groupchat") {
-			clm.meowerRequest({
-				cmd: "direct",
-				val: {
-					cmd: "set_chat_state",
-					val: {
-						state: 0,
-						chatid: $chatid,
-					},
-				},
-			});
-		}
-		chatid.set("");
-		page.set("blank");
-		tick().then(() => page.set(newPage));
-	}
-	var whusername = "User"
-	var whpost = ""
-	function whsend() {
-		console.log(`${whusername}:${whpost}`)
-		fetch('https://webhooks.meower.org/post/home', {
-			method: 'POST',
-			headers: {
-				'Accept': 'application/json',
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify({ "post": whpost,"username":whusername })
-		})
-		.then(response => response.text())
-		.then(response => console.log(response))
-	}
+// 	function goto(newPage, resetScroll = true) {
+// 		if (
+// 			!$user.name &&
+// 			newPage !== "home" &&
+// 			newPage !== "settings" &&
+// 			newPage !== "changelog" &&
+// 			newPage !== "search"
+// 		) {
+// 			modalPage.set("signup");
+// 			modalShown.set(true);
+// 			return;
+// 		}
+// 		if (resetScroll) {
+// 			window.scrollTo(0, 0);
+// 		}
+// 		if ($page === "groupchat") {
+// 			clm.meowerRequest({
+// 				cmd: "direct",
+// 				val: {
+// 					cmd: "set_chat_state",
+// 					val: {
+// 						state: 0,
+// 						chatid: $chatid,
+// 					},
+// 				},
+// 			});
+// 		}
+// 		chatid.set("");
+// 		page.set("blank");
+// 		tick().then(() => page.set(newPage));
+// 	}
+// 	var whusername = "User"
+// 	var whpost = ""
+// 	function whsend() {
+// 		console.log(`${whusername}:${whpost}`)
+// 		fetch('https://webhooks.meower.org/post/home', {
+// 			method: 'POST',
+// 			headers: {
+// 				'Accept': 'application/json',
+// 				'Content-Type': 'application/json'
+// 			},
+// 			body: JSON.stringify({ "post": whpost,"username":whusername })
+// 		})
+// 		.then(response => response.text())
+// 		.then(response => console.log(response))
+// 	}
 	
 	import {
 		mainPage as page,
@@ -144,7 +144,7 @@
 		<h1>Developer tools</h1>
 		Hello everyone its THE DEV TOOLS HERE back with another youtube video!!!!!!
 	</Container>
-	<Container style="height: 150px;">
+<!-- 	<Container style="height: 150px;">
 		<div class="settings-controls">
             <input bind:value={whusername} type="text" id="username" style="margin-bottom:4px">
         	<textarea bind:value={whpost} rows="4" class="container type-message" style="resize: none;width:calc(100% - (11px * 2) - 100px)"></textarea>
@@ -195,7 +195,7 @@
 		</div>
 		<h1>Go to page</h1>
 		<input bind:value={target_page}>
-	</Container>
+	</Container> -->
 	
 </Modal>
 <!-- 	<Container>
