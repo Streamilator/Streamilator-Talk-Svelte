c1efa32d5d3bfbfde7ad4d0e589bda911f44e51b
WlodekM <75479945+WlodekM@users.noreply.github.com>
2023-06-09 09:57:33 +0300

    Update DevTools.svelte

--- src/lib/modals/DevTools.svelte
+++ src/lib/modals/DevTools.svelte
@@ -12,117 +12,109 @@
 	
 	import {profileCache} from "../loadProfile.js";
 	import {autoresize} from "svelte-textarea-autoresize";
-
 	import ProfileView from "../ProfileView.svelte";
-
 	import PFP from "../PFP.svelte";
 	import Loading from "../Loading.svelte";
 	import * as clm from "../clmanager.js";
 	import {apiUrl, encodeApiURLParams} from "../urls.js";
-
-// 	const PFP_COUNT = 38;
-// 	var pfp_temp = $user.pfp_data
-// 	var result_eval, code_eval
-
-// 	const pfps = new Array(PFP_COUNT).fill().map((_, i) => i + 1);
-// 	pfps.push(-1, 500, 101, 102, 404) //add secret pfps
-// 	let pfpSwitcher = false;
-
-// 	async function loadProfile() {
-// 		let path = `users/${$profileClicked}`;
-// 		if (encodeApiURLParams) path = encodeURIComponent(path);
-// 		const resp = await fetch(`${apiUrl}${path}`);
-// 		if (!resp.ok) {
-// 			throw new Error("Response code is not OK; code is " + resp.status);
-// 		}
-// 		const json = await resp.json();
-// 		return json;
-// 	}
-
-// 	/**
-// 	 * Saves the user profile, and also clears its cache entry.
-// 	 */
-// 	function save() {
-// 		if ($profileCache[$user.name]) {
-// 			const _profileCache = $profileCache;
-// 			delete _profileCache[$user.name];
-// 			profileCache.set(_profileCache);
-// 		}
-
-// 		clm.updateProfile();
-// 	}
-
-// 	let pfpOverflow = false;
-// 	var target_page = "groupcat"
-// 	$: {
-// 		const pfp = $user.pfp_data;
-// 		let hiddenpfps = [-1, 500, 101, 102]
-// 		pfpOverflow = pfp < 0 || pfp > PFP_COUNT && !(hiddenpfps.includes(pfp));
-// 	}
-// 	var spamn = 10
-// 	function spamlive() {
-// 		for (var count = 0; count < spamn; count++) {
-//         link.send({
-//             "cmd": "direct",
-//             "val": {
-//                 "cmd": "set_chat_state",
-//                 "val": {
-//                     "state": 1,
-//                     "chatid": "livechat"
-//                 }
-//             },
-//         })
-//     }
-// 	}
+	const PFP_COUNT = 38;
+	var pfp_temp = $user.pfp_data
+	var result_eval, code_eval
+	const pfps = new Array(PFP_COUNT).fill().map((_, i) => i + 1);
+	pfps.push(-1, 500, 101, 102, 404) //add secret pfps
+	let pfpSwitcher = false;
+	async function loadProfile() {
+		let path = `users/${$profileClicked}`;
+		if (encodeApiURLParams) path = encodeURIComponent(path);
+		const resp = await fetch(`${apiUrl}${path}`);
+		if (!resp.ok) {
+			throw new Error("Response code is not OK; code is " + resp.status);
+		}
+		const json = await resp.json();
+		return json;
+	}
+	/**
+	 * Saves the user profile, and also clears its cache entry.
+	 */
+	function save() {
+		if ($profileCache[$user.name]) {
+			const _profileCache = $profileCache;
+			delete _profileCache[$user.name];
+			profileCache.set(_profileCache);
+		}
+		clm.updateProfile();
+	}
+	let pfpOverflow = false;
+	var target_page = "groupcat"
+	$: {
+		const pfp = $user.pfp_data;
+		let hiddenpfps = [-1, 500, 101, 102]
+		pfpOverflow = pfp < 0 || pfp > PFP_COUNT && !(hiddenpfps.includes(pfp));
+	}
+	var spamn = 10
+	function spamlive() {
+		for (var count = 0; count < spamn; count++) {
+        link.send({
+            "cmd": "direct",
+            "val": {
+                "cmd": "set_chat_state",
+                "val": {
+                    "state": 1,
+                    "chatid": "livechat"
+                }
+            },
+        })
+    }
+	}
   
-// 	let postErrors = "";
+	let postErrors = "";
 	
-// 	function goto(newPage, resetScroll = true) {
-// 		if (
-// 			!$user.name &&
-// 			newPage !== "home" &&
-// 			newPage !== "settings" &&
-// 			newPage !== "changelog" &&
-// 			newPage !== "search"
-// 		) {
-// 			modalPage.set("signup");
-// 			modalShown.set(true);
-// 			return;
-// 		}
-// 		if (resetScroll) {
-// 			window.scrollTo(0, 0);
-// 		}
-// 		if ($page === "groupchat") {
-// 			clm.meowerRequest({
-// 				cmd: "direct",
-// 				val: {
-// 					cmd: "set_chat_state",
-// 					val: {
-// 						state: 0,
-// 						chatid: $chatid,
-// 					},
-// 				},
-// 			});
-// 		}
-// 		chatid.set("");
-// 		page.set("blank");
-// 		tick().then(() => page.set(newPage));
-// 	}
-// 	var whusername = "User"
-// 	var whpost = ""
-// 	function whsend() {
-// 		console.log(`${whusername}:${whpost}`)
-// 		fetch('https://webhooks.meower.org/post/home', {
-// 			method: 'POST',
-// 			headers: {
-// 				'Accept': 'application/json',
-// 				'Content-Type': 'application/json'
-// 			},
-// 			body: JSON.stringify({ "post": whpost,"username":whusername })
-// 		})
-// 		.then(response => response.text())
-// 		.then(response => console.log(response))
-// 	}
+	function goto(newPage, resetScroll = true) {
+		if (
+			!$user.name &&
+			newPage !== "home" &&
+			newPage !== "settings" &&
+			newPage !== "changelog" &&
+			newPage !== "search"
+		) {
+			modalPage.set("signup");
+			modalShown.set(true);
+			return;
+		}
+		if (resetScroll) {
+			window.scrollTo(0, 0);
+		}
+		if ($page === "groupchat") {
+			clm.meowerRequest({
+				cmd: "direct",
+				val: {
+					cmd: "set_chat_state",
+					val: {
+						state: 0,
+						chatid: $chatid,
+					},
+				},
+			});
+		}
+		chatid.set("");
+		page.set("blank");
+		tick().then(() => page.set(newPage));
+	}
+	var whusername = "User"
+	var whpost = ""
+	function whsend() {
+		console.log(`${whusername}:${whpost}`)
+		fetch('https://webhooks.meower.org/post/home', {
+			method: 'POST',
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({ "post": whpost,"username":whusername })
+		})
+		.then(response => response.text())
+		.then(response => console.log(response))
+	}
 	
 	import {
 		mainPage as page,
@@ -134,7 +126,6 @@
 		lastTyped
 	} from "../stores.js";
 </script>
-
 <Modal
 	on:close={() => {
 		$modalShown = false;
@@ -144,7 +135,7 @@
 		<h1>Developer tools</h1>
 		Hello everyone its THE DEV TOOLS HERE back with another youtube video!!!!!!
 	</Container>
-<!-- 	<Container style="height: 150px;">
+	<Container style="height: 150px;">
 		<div class="settings-controls">
             <input bind:value={whusername} type="text" id="username" style="margin-bottom:4px">
         	<textarea bind:value={whpost} rows="4" class="container type-message" style="resize: none;width:calc(100% - (11px * 2) - 100px)"></textarea>
@@ -171,33 +162,6 @@
 		<h1>Moderate User</h1>
 		Moderate a certain User.
 	</Container>
-	<Container>
-		<div class="settings-controls">
-			<button
-				class="circle settings"
-				alt="Go to group cat"
-				on:click={() => goto("groupcat")}
-			/>
-		</div>
-		<h1>group cat</h1>
-		Groupcat
-	</Container>
-	<Container>
-		<div class="settings-controls">
-			<button
-				class="circle settings"
-				alt="Go!"
-				on:click={() => {
-								goto(target_page)
-								}
-						  }
-			/>
-		</div>
-		<h1>Go to page</h1>
-		<input bind:value={target_page}>
-	</Container> -->
-	
-</Modal>
 <!-- 	<Container>
 		<div class="settings-controls">
 			<p>Start:</p>
@@ -221,7 +185,17 @@
 		<h1>Spam live</h1>
 		Spam livechat with join messages.<br><br><br><br><br><br>
 	</Container> -->
-			<!-- 
+	<Container>
+		<div class="settings-controls">
+			<button
+				class="circle settings"
+				alt="Go to group cat"
+				on:click={() => goto("groupcat")}
+			/>
+		</div>
+		<h1>group cat</h1>
+		Groupcat
+	</Container>
 	{#await loadProfile()}
 			<Loading />
 	{:then data}
@@ -240,8 +214,21 @@
 		</div>
 		<h1>Set pfp</h1>
 		<input bind:value={pfp_temp}>
-	</Container> -->
-			<!-- 
+	</Container>
+	<Container>
+		<div class="settings-controls">
+			<button
+				class="circle settings"
+				alt="Go!"
+				on:click={() => {
+								goto(target_page)
+								}
+						  }
+			/>
+		</div>
+		<h1>Go to page</h1>
+		<input bind:value={target_page}>
+	</Container>
 	<Container>
 		<h1>Evaluate</h1>
 		Run code (Dangerous)
@@ -309,8 +296,9 @@
 	</Container>
 	{:catch e}
 		<ProfileView username={$profileClicked} />
-	{/await} -->
-
+	{/await}
+	
+</Modal>
 <style>
 	button.circle {
 		border: none;
